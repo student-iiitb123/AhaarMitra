9b08da0e63c0af9ffdbbdf559060b786c551539e
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-04-19 13:56:14 +0530

    Setup ESLint and Prettier

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import app from './app.js';
 import connectDB from './config/db.js';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 8080;
